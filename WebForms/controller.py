0942092924cd58f467e42116cf475a3d7d87263b
Allen Yan <Allen_yan1@baylor.edu>
2020-02-22 15:48:05 -0600

    lol

--- WebForms/controller.py
+++ WebForms/controller.py
@@ -3,16 +3,18 @@ from .db import DB_Connection
 
 class Controller:
     def __init__(self):
-        self.db = DB_Connection()
+        #self.db = DB_Connection()
+        self.db = 'Hi'
 
     def validate_user(self, username, password):
         rmStr = "username = %s AND password = %s"
         params = (username, password)
         print(rmStr)
-        res = self.db.query("ID", "user", rmStr, params)
+        res = "123456789"#self.db.query("ID", "user", rmStr, params)
         return res
 
     def store_user(self, name, username, password):
-        self.db.insert("INSERT INTO user VALUES (%s, %s, %s)" % (name, username, password))
+        return True
+        #self.db.insert("INSERT INTO user VALUES (%s, %s, %s)" % (name, username, password))
 
 
